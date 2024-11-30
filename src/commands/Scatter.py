80b073753f1e08326d698055226b7011983d2dda
Francesco Bertolotti <f14.bertolotti@gmail.com>
2024-11-30 22:09:46 +0100

    added linewidth option

--- src/commands/Scatter.py
+++ src/commands/Scatter.py
@@ -11,16 +11,21 @@ def scatter(
         samples,
         label, 
         color, 
+        linewidth,
         x, 
         y,
-        ax
+        ax,
+        where,
+        legend
     ):
-    data = load_data(paths = input_paths, samples = samples)
+    data = load_data(paths = input_paths, samples = samples, where = where)
     seaborn.scatterplot(
         data  = data,
         x     = x,
         y     = y,
         color = color,
+        linewidth = linewidth,
         label = label if label else os.path.basename(input_paths[0]),
+        legend= False if legend == "none" else legend,
         ax    = plotobj.axs[ax[0]][ax[1]]
     )
