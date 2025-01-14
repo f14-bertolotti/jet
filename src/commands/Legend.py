1616195b1c354dcfd583c5c103ebc73579f40850
Francesco Bertolotti <f14.bertolotti@gmail.com>
2025-01-14 14:54:23 +0100

    added style to legend

--- src/commands/Legend.py
+++ src/commands/Legend.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import click
 
 @click.group(invoke_without_command=True)
-@click.option("--line", "lines", type=(str,float,float,float,float), default=[], multiple=True, help="add line to legend in the form of (label,color,width)")
+@click.option("--line", "lines", type=(str,float,float,float,float,str), default=[], multiple=True, help="add line to legend in the form of (label,color,width,style)")
 @click.option("--frameon", "frameon", type=bool, default=False, help="True if the legend frame should be showed")
 @click.pass_obj
-def legend(plotobj, lines,frameon):
-    custom_handles = [plt.Line2D([0], [0], color=(colorx, colory, colorz), lw=lw, label=label) for label, colorx, colory, colorz, lw in lines]
+def legend(plotobj, lines, frameon):
+    custom_handles = [plt.Line2D([0], [0], color=(colorx, colory, colorz), lw=lw, label=label, linestyle=style) for label, colorx, colory, colorz, lw, style in lines]
     plt.legend(handles=custom_handles, loc='best', frameon=frameon)
