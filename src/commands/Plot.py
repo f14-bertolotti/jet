1bebc92a559d624bee8f23da5a1a3101bda92bc4
Francesco Bertolotti <f14.bertolotti@gmail.com>
2025-01-24 14:45:03 +0100

    figsize default is none

--- src/commands/Plot.py
+++ src/commands/Plot.py
@@ -5,12 +5,12 @@ import click
 @click.pass_obj
 @click.option("--show"         , "show"         , type = bool         , default = True   , help = "True if the plot has to be shown.")
 @click.option("--output-path"  , "output_path"  , type = click.Path() , default = None   , help = "Output plot path.")
-@click.option("--figsize"      , "figsize"      , type = (float,float), default = (10,10), help = "Figure size.")
+@click.option("--figsize"      , "figsize"      , type = (float,float), default = (None,None), help = "Figure size.")
 @click.option("--tight"        , "tight"        , type = bool         , default = False  , help = "Tight layout.")
 def plot(plotobj, show, output_path, figsize, tight):
     if tight: plt.tight_layout()
     if show: plt.show()
-    plotobj.fig.set_figheight(figsize[0])
-    plotobj.fig.set_figwidth(figsize[1])
+    if figsize[0] is not None: plotobj.fig.set_figheight(figsize[0])
+    if figsize[1] is not None: plotobj.fig.set_figwidth(figsize[1])
     if output_path: plotobj.fig.savefig(output_path)
 
